ce273ab7d923d9b64973a84b3ff95d306cfc5e71
Tonmoy Mohajan <tonmoymohajan01@gmail.com>
2026-07-14 16:57:35 +0600

    Add Beecrowd 1038 Solution

---
+++ Beecrowd_Problem/Snack.py
@@ -0,0 +1,12 @@
+x, y = map(int,input().split())
+
+if x == 1:
+    print(f'Total: R$ {y*4.00:.2f}')
+if x == 2:
+    print(f'Total: R$ {y*4.50:.2f}')
+if x == 3:
+    print(f'Total: R$ {y*5.00:.2f}')
+if x == 4:
+    print(f'Total: R$ {y*2.00:.2f}')  
+if x == 5:
+    print(f'Total: R$ {y*1.50:.2f}')  

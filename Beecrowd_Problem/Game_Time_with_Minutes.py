2b26fce461ef957936f98406137d8e028aed7bdb
Tonmoy Mohajan <tonmoymohajan01@gmail.com>
2026-07-16 05:21:06 +0600

    Optimize beecrowd 1047 solution

--- Beecrowd_Problem/Game_Time_with_Minutes.py
+++ Beecrowd_Problem/Game_Time_with_Minutes.py
@@ -1,22 +1,14 @@
-h1, m1, h2, m2 = map(int,input().split())
+h1, m1, h2, m2 = map(int, input().split())
 
-if h1 > h2 and m1 > m2:
-    print(f'O JOGO DUROU {((24-h1)+h2)-1} HORA(S) E {(60-m1)+m2} MINUTO(S)')
-elif h1 > h2 and m1 < m2:
-    print(f'O JOGO DUROU {(24-h1)+h2} HORA(S) E {m2-m1} MINUTO(S)')
-elif h1 > h2 and m1 == m2:
-    print(f'O JOGO DUROU {(24-h1)+h2} HORA(S) E 0 MINUTO(S)')
+start = h1 * 60 + m1
+end = h2 * 60 + m2
 
-elif h2 > h1  and m1 > m2:
-    print(f'O JOGO DUROU {(h2-h1) -} HORA(S) E {(60-m1)+m2} MINUTO(S)')
-elif h2 > h1 and m1 < m2:
-    print(f'O JOGO DUROU {h2-h1} HORA(S) E {m2-m1} MINUTO(S)')
-elif h2 > h1 and m1 == m2:
-    print(f'O JOGO DUROU {h2-h1} HORA(S) E 0 MINUTO(S)')
+if start >= end:
+    end += 24 * 60
 
-elif h2 == h1 and m1 > m2:
-    print(f'O JOGO DUROU 24 HORA(S) E {(60-m1)+m2} MINUTO(S)')
-elif h2 == h1 and m1 < m2:
-    print(f'O JOGO DUROU 24 HORA(S) E {m2-m1} MINUTO(S)')
-elif h2 == h1 and m1 == m2:
-    print(f'O JOGO DUROU  HORA(S) E 0 MINUTO(S)')
+duration = end - start
+
+hour = duration // 60
+minute = duration % 60
+
+print(f"O JOGO DUROU {hour} HORA(S) E {minute} MINUTO(S)")

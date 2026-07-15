7bbab2749ddec428cd5c9a3d3ae5dfea548ce9ff
Tonmoy Mohajan <tonmoymohajan01@gmail.com>
2026-07-16 05:22:42 +0600

    Add Beecrowd 1046 Solution

---
+++ Beecrowd_Problem/Game_Time.py
@@ -0,0 +1,8 @@
+a, b = map(int,input().split())
+
+if a > b:
+    print(f'O JOGO DUROU {(24-a)+b} HORA(S)')
+elif a < b:
+    print(f'O JOGO DUROU {b-a} HORA(S)')
+elif a == b:
+    print(f'O JOGO DUROU 24 HORA(S)')

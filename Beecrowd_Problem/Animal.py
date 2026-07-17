160d71f33f4443d9ad5e760157e9883379392253
Tonmoy Mohajan <tonmoymohajan01@gmail.com>
2026-07-18 01:43:46 +0600

    Add Beecrowd 1049 Solution

---
+++ Beecrowd_Problem/Animal.py
@@ -0,0 +1,20 @@
+a1 = input()
+a2 = input()
+a3 = input()
+
+if a1 == "vertebrado" and a2 == "ave" and a3 == "carnivoro":
+    print("aguia")
+elif a1 == "vertebrado" and a2 == "ave" and a3 == "onivoro":
+    print("pomba")
+elif a1 == "vertebrado" and a2 == "mamifero" and a3 == "onivoro":
+    print("homem")
+elif a1 == "vertebrado" and a2 == "mamifero" and a3 == "herbivoro":
+    print("vaca")
+elif a1 == "invertebrado" and a2 == "inseto" and a3 == "hematofago":
+    print("pulga")
+elif a1 == "invertebrado" and a2 == "inseto" and a3 == "herbivoro":
+    print("lagarta")
+elif a1 == "invertebrado" and a2 == "anelideo" and a3 == "hematofago":
+    print("sanguessuga")
+elif a1 == "invertebrado" and a2 == "anelideo" and a3 == "onivoro":
+    print("minhoca")

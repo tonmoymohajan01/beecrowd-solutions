ede7ba7daca971e447be865cf0cd7b6026b7c746
Tonmoy Mohajan <tonmoymohajan01@gmail.com>
2026-07-18 01:37:02 +0600

    Optimize Month Solution

--- Beecrowd_Problem/Month.py
+++ Beecrowd_Problem/Month.py
@@ -3,6 +3,4 @@ n = int(input())
 m =["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 
-for i in range(1,13):
-    if n == i:
-        print(m[i-1])
+print(m[n-1])
